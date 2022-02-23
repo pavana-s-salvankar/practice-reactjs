7eb5b5f5ec016df55e16297c2c37ab389edb2ca5
Pavana S Salvankar <pavanasalvankarbelle1999@gmail.com>
2022-02-23 15:47:06 +0530

    react-dom-manipulation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import ErrorBoudary from './Errorboundary';
 import Hero from './Components/Color';
-import styled from 'styled-components';
 import Navbar from './Components/Navbar';
-import './Styles/navbar.scss';
+import './Styles/app.scss';
+import ReactDOMManipulation from './Components/ReactDOMManipulation';
+import React, { useState } from 'react';
 // Error boundary cannot be functional component
 function App() {
   const array = ['black', 'lightgreen', 'lightblue', 'white', 'blue', 'red', 'green', 'purple'];
   return (
-    <Container>
+    <div className='appContainer'>
       <Navbar />
       {array.map((item) => (
         <ErrorBoudary key={item}>
           <Hero colorname={item}></Hero>
         </ErrorBoudary>
       ))}
-      <button className='myButton'>You can click me</button>
-    </Container>
+      <ReactDOMManipulation />
+    </div>
   );
 }
 
 export default App;
-const Container = styled.div`
-  width: 100%;
-  height: 100%;
-`;
---
+++ src/Components/ReactDOMManipulation.js
@@ -0,0 +1,26 @@
+import React, { useRef, useState } from 'react';
+import '../Styles/reactdom.scss';
+const Comp = () => {
+  const nameInput = useRef();
+  const [isCircle, setIsCircle] = useState(true);
+  const clearInput = () => {
+    nameInput.current.value = '';
+  };
+  return (
+    <>
+      <div className='flexRow'>
+        <div className={isCircle ? 'circle' : 'square'}></div>
+        <button className='myButton' onClick={() => setIsCircle(!isCircle)}>
+          CHANGE SHAPE
+        </button>
+      </div>
+      <div className='flexRow'>
+        <input ref={nameInput} type='text' placeholder='Enter...'></input>
+        <button className='myButton' type='submit' onClick={clearInput}>
+          Submit
+        </button>
+      </div>
+    </>
+  );
+};
+export default Comp;
